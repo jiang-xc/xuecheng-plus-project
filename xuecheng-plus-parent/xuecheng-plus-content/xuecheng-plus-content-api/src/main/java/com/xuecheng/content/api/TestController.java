3e21b41b949653ae971340add987fc8b08f309e4
jiang-xc <1595081190@qq.com>
2023-04-26 03:25:30 +0800

    搭建nacos，完善了内容管理模块和系统管理模块的配置管理

--- xuecheng-plus-parent/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TestController.java
+++ xuecheng-plus-parent/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TestController.java
@@ -4,14 +4,19 @@ package com.xuecheng.content.api;
 import com.xuecheng.base.exception.CommonError;
 import com.xuecheng.base.exception.XueChengPlusException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RefreshScope
 public class TestController {
 
 
+    @Value("${common.config.testV}")
+    private String testV;
 
     @PostMapping("/course/test")
     public String testSwag(String arg1){
@@ -22,8 +27,8 @@ public class TestController {
             XueChengPlusException.cast(CommonError.OBJECT_NULL.getErrMessage());
         }
 
-        int i = 1/0;
+        //int i = 1/0;
 
-        return "success";
+        return testV;
     }
 }
